25716f3d3214f051a3578e5b56e0cc44b2fffefb
Amara Khan <amaraskhan@gmail.com>
2023-08-29 13:03:28 -0400

    adding air pollution data in specdata dir and initial analysis file

---
+++ air_pollution.R
@@ -0,0 +1,10 @@
+pollutantmean <- function(dir, pollutant, id=1:322) {
+  # dir : 
+  # - char vector of length 1
+  # - indicating the location of the CSV files
+
+  # pollutant : 
+  # - char vector of length 1
+  # - indicating the name of the pollutant for which we will calculate the mean
+  # - either "sulfate" or "nitrate"
+}
