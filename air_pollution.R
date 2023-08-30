3865c41dd2e2291636765fe486a32209ded23e21
Amara Khan <amaraskhan@gmail.com>
2023-08-29 23:49:51 -0400

    adding import and function call tests to air_pollution.R

--- air_pollution.R
+++ air_pollution.R
@@ -1,10 +1,5 @@
-pollutantmean <- function(dir, pollutant, id=1:322) {
-  # dir : 
-  # - char vector of length 1
-  # - indicating the location of the CSV files
+print(R.version.string)
+source("pollutantmean.R")
 
-  # pollutant : 
-  # - char vector of length 1
-  # - indicating the name of the pollutant for which we will calculate the mean
-  # - either "sulfate" or "nitrate"
-}
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
